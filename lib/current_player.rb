dd53e558c7a759c4c685640239d9612a4504e660
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-28 00:22:47 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@
 def turn_count(board)
   board.each do |space|
     counter = 0
-    if (space == "X" || (space == "O")
+    if (space == "X" || space == "O")
       counter+=1
     return counter
   end
